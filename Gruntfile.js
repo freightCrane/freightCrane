bff1cc3b0c8f38768430ae639cf342171a58bd95
Stefan Wallin <stefan.w@festiz.com>
2014-04-02 21:29:34 +0200

    Added missing .travis.yml-file and defined how to run the test script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,6 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.registerTask('test', 'qunit:src');
-	grunt.registerTask('travis', ['test']);
 
 	// Default task(s).
 	grunt.registerTask('default', ['test']);
