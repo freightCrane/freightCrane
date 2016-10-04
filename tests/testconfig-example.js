32c7234f8db80fc023394e4e1e9d2358a94a84c9
Stefan Wallin <stefan.w@festiz.com>
2016-10-04 23:18:13 +0200

    Make sure tests work locally for remote providers as well

---
+++ tests/testconfig-example.js
@@ -0,0 +1,7 @@
+var testCredentials = {
+  'requireSecure': true, // This can be false on localhost if you are unable use SSL in development
+  'dropbox_appKey': 'appkey',
+  'github_repo': 'githubsusername/reponame',
+  'github_token': '<place your personal token here>',
+  'github_tokenService': '<place your personal token here>'
+};
